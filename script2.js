e69fe47022e288fe0dce130ba0f53286cf465507
Vaishnavi Gupta <97463858+vaishnavi402@users.noreply.github.com>
2023-08-18 19:53:38 +0530

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,10 @@
+var MainImg = document.getElementById("MainImg");
+var b2 = document.getElementsByClassName("b2");
+
+b2[0] = onclick = function(){
+    MainImg.src = b2[0].src
+}
+
+b2[1] = onclick = function(){
+    MainImg.src = b2[1].src
+}
